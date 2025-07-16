da348862b8a91c9b0e0984222e9f0c563fb14413
Akshat577 <akshatjain5773@gmail.com>
2025-07-16 11:24:10 +0530

    Created Footer for Educator Dashboard

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Educator/Navbar";
 import Sidebar from "../../components/Educator/Sidebar";
+import Footer from "../../components/Educator/Footer";
 
 
 const Educator = () => {
@@ -13,6 +14,8 @@ const Educator = () => {
         <Sidebar />
         <div className="flex-1">{<Outlet />}</div>
       </div>
+
+      <Footer />
 
     </div>
   );
